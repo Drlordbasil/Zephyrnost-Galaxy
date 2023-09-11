173ac2d8815872eba5d836c86f3d36403dbf6f3c
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:08:05 -0400

    AI-enhanced and auto-formatted code

--- AI_CoPilot.py
+++ AI_CoPilot.py
@@ -1,70 +1,5 @@
 import os
-Here are some optimizations for the Python script:
-
-1. Import only the necessary modules(os) instead of importing everything from the module:
-```python
-```
-
-2. Use a constant variable for the name of the program to run(e.g., "main.py") to avoid hardcoding it multiple times:
-```python
-PROGRAM_NAME = "main.py"
-```
-
-3. Use a loop to repeatedly ask for user input instead of running the program only once:
-```python
-
-
-def main():
-    print("Welcome to the Program Runner Chatbot!")
-    print("This chatbot will help you run the 'main.py' program locally on your PC.")
-
-    while True:
-        print("\n======== MENU =======")
-        print("1. Run Program")
-        print("0. Exit")
-
-        choice = input("Enter your choice: ")
-
-        try:
-            choice = int(choice)
-        except ValueError:
-            print("Invalid choice. Please try again.")
-            continue
-
-        if choice == 1:
-            run_program()
-        elif choice == 0:
-            print("Exiting chatbot...")
-            break
-        else:
-            print("Invalid choice. Please try again.")
-
-
-```
-
-4. Simplify the logic for checking if the program file exists using the `os.path.isfile` function instead of `os.listdir()`:
-```python
-
-
-def run_program():
-    # Check if main.py exists in the current directory
-    if not os.path.isfile(PROGRAM_NAME):
-        print(f"{PROGRAM_NAME} file not found in the current directory.")
-        return
-
-    # Run main.py
-    os.system(f"python {PROGRAM_NAME}")
-
-
-```
-
-5. Add a guard clause to handle the case where the script is imported as a module:
-```python
-if __name__ == "__main__":
-    main()
-```
-
-Here is the optimized code:
+Thank you for the suggestions! Here is the optimized code with the improvements you provided:
 
 ```python
 
@@ -72,12 +7,10 @@ PROGRAM_NAME = "main.py"
 
 
 def run_program():
-    # Check if main.py exists in the current directory
     if not os.path.isfile(PROGRAM_NAME):
         print(f"{PROGRAM_NAME} file not found in the current directory.")
         return
 
-    # Run main.py
     os.system(f"python {PROGRAM_NAME}")
 
 
@@ -90,10 +23,8 @@ def main():
         print("1. Run Program")
         print("0. Exit")
 
-        choice = input("Enter your choice: ")
-
         try:
-            choice = int(choice)
+            choice = int(input("Enter your choice: "))
         except ValueError:
             print("Invalid choice. Please try again.")
             continue
@@ -111,4 +42,4 @@ if __name__ == "__main__":
     main()
 ```
 
-Please note that these optimizations enhance the readability and maintainability of the code but may not necessarily improve its performance.
+I hope this helps optimize the code! Let me know if you need any further assistance.
