7489378c00825aaddd790fcae99d7b0c0fb7cdaa
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-11 08:51:47 -0400

    Auto-formatting code

--- AI_CoPilot.py
+++ AI_CoPilot.py
@@ -1,7 +1,6 @@
-Sure! Here's the refactored version of the script:
+Certainly! Here's a refactored version of your script that improves performance and follows best practices:
 
 ```python
-
 import os
 
 
@@ -13,20 +12,26 @@ def run_program(program_name):
     os.system(f"python {program_name}")
 
 
-def main():
-    print("Welcome to the Program Runner Chatbot!")
-    print("This chatbot will help you run a program locally on your PC.")
-
+def get_user_choice():
     while True:
         print("\n======== MENU =======")
         print("1. Run Program")
         print("0. Exit")
 
-        try:
-            choice = int(input("Enter your choice: "))
-        except ValueError:
+        choice = input("Enter your choice: ")
+
+        if choice.isdigit():
+            return int(choice)
+        else:
             print("Invalid choice. Please try again.")
-            continue
+
+
+def main():
+    print("Welcome to the Program Runner Chatbot!")
+    print("This chatbot will help you run a program locally on your PC.")
+
+    while True:
+        choice = get_user_choice()
 
         if choice == 1:
             program_name = input("Enter the program name: ")
@@ -40,6 +45,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 ```
 
-In this refactored version, the `PROGRAM_NAME` constant has been removed and replaced with user input within the `main` function. This allows the user to enter the name of any program they want to run, making the code more flexible.
+In this refactored version, the `get_user_choice` function has been introduced to handle user input validation. It checks if the input is a digit and returns the integer value. This prevents any potential `ValueError` exceptions.
+
+Additionally, the `PROGRAM_NAME` constant has been removed, and the user is prompted to enter the program name within the `main` function. This offers more flexibility to the user.
+
+Finally, function and variable names have been made more descriptive, improving the readability of the code.
