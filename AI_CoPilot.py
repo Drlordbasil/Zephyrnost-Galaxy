022ee3214ff5547527d6511d88b2fa1c63ba4bac
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:04:11 -0400

    AI-enhanced and auto-formatted code

--- AI_CoPilot.py
+++ AI_CoPilot.py
@@ -1,17 +1,84 @@
 import os
-Here is an AI chatbot program that helps the user run the "main.py" program locally on their PC:
+Here are some optimizations for the Python script:
 
+1. Import only the necessary modules(os) instead of importing everything from the module:
+```python
+```
+
+2. Use a constant variable for the name of the program to run(e.g., "main.py") to avoid hardcoding it multiple times:
+```python
+PROGRAM_NAME = "main.py"
+```
+
+3. Use a loop to repeatedly ask for user input instead of running the program only once:
+```python
+
+
+def main():
+    print("Welcome to the Program Runner Chatbot!")
+    print("This chatbot will help you run the 'main.py' program locally on your PC.")
+
+    while True:
+        print("\n======== MENU =======")
+        print("1. Run Program")
+        print("0. Exit")
+
+        choice = input("Enter your choice: ")
+
+        try:
+            choice = int(choice)
+        except ValueError:
+            print("Invalid choice. Please try again.")
+            continue
+
+        if choice == 1:
+            run_program()
+        elif choice == 0:
+            print("Exiting chatbot...")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+```
+
+4. Simplify the logic for checking if the program file exists using the `os.path.isfile` function instead of `os.listdir()`:
 ```python
 
 
 def run_program():
     # Check if main.py exists in the current directory
-    if "main.py" not in os.listdir():
-        print("main.py file not found in the current directory.")
+    if not os.path.isfile(PROGRAM_NAME):
+        print(f"{PROGRAM_NAME} file not found in the current directory.")
         return
 
     # Run main.py
-    os.system("python main.py")
+    os.system(f"python {PROGRAM_NAME}")
+
+
+```
+
+5. Add a guard clause to handle the case where the script is imported as a module:
+```python
+if __name__ == "__main__":
+    main()
+```
+
+Here is the optimized code:
+
+```python
+
+PROGRAM_NAME = "main.py"
+
+
+def run_program():
+    # Check if main.py exists in the current directory
+    if not os.path.isfile(PROGRAM_NAME):
+        print(f"{PROGRAM_NAME} file not found in the current directory.")
+        return
+
+    # Run main.py
+    os.system(f"python {PROGRAM_NAME}")
 
 
 def main():
@@ -44,4 +111,4 @@ if __name__ == "__main__":
     main()
 ```
 
-To use the AI chatbot, simply run the above Python program. It will provide a menu with options to run the "main.py" program or exit the chatbot.
+Please note that these optimizations enhance the readability and maintainability of the code but may not necessarily improve its performance.
