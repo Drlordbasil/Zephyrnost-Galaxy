edd171b91633d928c5e0e065a7a67016c79e4d7d
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-11 08:31:11 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ class ExpenseTracker:
         self.expenses = []
 
     def add_expense(self):
-        expense = input("Enter the expense amount: ")
-        self.expenses.append(float(expense))
+        expense = float(input("Enter the expense amount: "))
+        self.expenses.append(expense)
         print("Expense added successfully.")
 
     def visualize_expenses(self):
@@ -24,10 +24,8 @@ class ExpenseTracker:
 class BudgetOptimizer:
     def generate_budget(self):
         while True:
-            expenses = input(
-                "Enter your expected expenses (comma-separated): ")
             try:
-                expenses = [float(expense) for expense in expenses.split(",")]
+                expenses = [float(expense) for expense in input("Enter your expected expenses (comma-separated): ").split(",")]
                 total_expenses = sum(expenses)
                 print("Total expected expenses:", total_expenses)
                 break
@@ -56,8 +54,7 @@ class DebtManager:
         self.interest_rates = interest_rates
 
     def recommend_debt_management_strategy(self):
-        print(
-            "Recommend debt management strategy based on debt balances and interest rates.")
+        print("Recommend debt management strategy based on debt balances and interest rates.")
 
 
 class TaxOptimizer:
@@ -88,20 +85,51 @@ class FinancialPlanner:
         self.tax_optimizer = TaxOptimizer(income=0, expenses=None)
         self.real_time_insights = RealTimeInsights()
 
+    def display_menu(self):
+        print("\n======= MENU =======")
+        print("1. Add Expense")
+        print("2. Visualize Expenses")
+        print("3. Generate Budget")
+        print("4. Analyze Spending")
+        print("5. Recommend Investments")
+        print("6. Optimize Portfolio")
+        print("7. Recommend Debt Management Strategy")
+        print("8. Optimize Tax Payments")
+        print("9. Generate Real-Time Insights")
+        print("0. Exit")
+
+    def process_choice(self, choice):
+        if choice == 1:
+            self.expense_tracker.add_expense()
+        elif choice == 2:
+            self.expense_tracker.visualize_expenses()
+        elif choice == 3:
+            self.budget_optimizer.generate_budget()
+        elif choice == 4:
+            self.budget_optimizer.analyze_spending(
+                self.expense_tracker.expenses)
+        elif choice == 5:
+            self.investment_advisor.recommend_investments()
+        elif choice == 6:
+            self.investment_advisor.optimize_portfolio()
+        elif choice == 7:
+            self.debt_manager.recommend_debt_management_strategy()
+        elif choice == 8:
+            self.tax_optimizer.optimize_tax_payments()
+        elif choice == 9:
+            self.real_time_insights.generate_insights()
+        elif choice == 0:
+            print("Exiting program...")
+            time.sleep(2)
+            return False
+        else:
+            print("Invalid choice. Please try again.")
+
+        return True
+
     def main(self):
         while True:
-            print("\n======= MENU =======")
-            print("1. Add Expense")
-            print("2. Visualize Expenses")
-            print("3. Generate Budget")
-            print("4. Analyze Spending")
-            print("5. Recommend Investments")
-            print("6. Optimize Portfolio")
-            print("7. Recommend Debt Management Strategy")
-            print("8. Optimize Tax Payments")
-            print("9. Generate Real-Time Insights")
-            print("0. Exit")
-
+            self.display_menu()
             choice = input("Enter your choice: ")
 
             try:
@@ -110,31 +138,8 @@ class FinancialPlanner:
                 print("Invalid choice. Please try again.")
                 continue
 
-            if choice == 1:
-                self.expense_tracker.add_expense()
-            elif choice == 2:
-                self.expense_tracker.visualize_expenses()
-            elif choice == 3:
-                self.budget_optimizer.generate_budget()
-            elif choice == 4:
-                self.budget_optimizer.analyze_spending(
-                    self.expense_tracker.expenses)
-            elif choice == 5:
-                self.investment_advisor.recommend_investments()
-            elif choice == 6:
-                self.investment_advisor.optimize_portfolio()
-            elif choice == 7:
-                self.debt_manager.recommend_debt_management_strategy()
-            elif choice == 8:
-                self.tax_optimizer.optimize_tax_payments()
-            elif choice == 9:
-                self.real_time_insights.generate_insights()
-            elif choice == 0:
-                print("Exiting program...")
-                time.sleep(2)
+            if not self.process_choice(choice):
                 break
-            else:
-                print("Invalid choice. Please try again.")
 
 
 if __name__ == '__main__':
